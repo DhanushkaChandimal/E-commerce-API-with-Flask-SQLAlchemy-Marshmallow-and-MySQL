cb85e7e465972e05f3c5640bc4cfb2c4fc8be924
Dhanushka <dhanushkamcr@gmail.com>
2025-08-27 16:26:08 -0500

    Configured app with database

---
+++ src/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:1234@localhost/ecommerce_api"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
