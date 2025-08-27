22ac0f7cf103412b72901409470d16ff6e84ca93
Dhanushka <dhanushkamcr@gmail.com>
2025-08-27 17:50:01 -0500

    Implemented schemas

--- src/app.py
+++ src/app.py
@@ -36,9 +36,31 @@ class Product(Base):
     product_name: Mapped[str] = mapped_column(String(200))
     price: Mapped[float] =  mapped_column(Float)
 
-order_product = Table{
+order_product = Table(
     "order_procuct",
     Base.metadata,
     Column("order_id", ForeignKey("orders.id"), primary_key=True),
     Column("product_id", ForeignKey("products.id"), primary_key=True)
-}
+)
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+
+class OrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Order
+        include_fk = True
+
+
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+order_schema = UserSchema()
+orders_schema = UserSchema(many=True)
+product_schema = UserSchema()
+products_schema = UserSchema(many=True)
