c35a72723737d21cdf3c99b2f7e4482e9c5761d0
Dhanushka <dhanushkamcr@gmail.com>
2025-08-27 16:57:54 -0500

    Implemented order model

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, DateTime, ForeignKey
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,12 +15,17 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-ma - Marshmallow(app)
+ma = Marshmallow(app)
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     address: Mapped[str] = mapped_column(String(150))
     email: Mapped[str] = mapped_column(String(100))
-    
+
+class Order(Base):
+    __tablename__ = "orders"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_date: Mapped[datetime] = mapped_column(DateTime)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
