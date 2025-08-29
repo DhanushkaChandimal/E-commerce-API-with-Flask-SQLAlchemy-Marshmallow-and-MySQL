e24412486dcc89e52202c8e2096bd3e9f22fd105
Dhanushka <dhanushkamcr@gmail.com>
2025-08-29 15:46:40 -0500

    Separate endpoints to api module

---
+++ src/api/orders_api.py
@@ -0,0 +1,75 @@
+from flask import request, jsonify
+from sqlalchemy import select, exc
+from marshmallow import ValidationError
+from config import app, db
+from models.user import User
+from models.order import Order
+from models.product import Product
+from schemas import products_schema, order_schema, orders_schema
+
+@app.route('/orders', methods = ['POST'])
+def create_order():
+    try:
+        order_data = order_schema.load(request.json)
+        new_order = Order(user_id=order_data['user_id'], order_date=order_data['order_date'])
+        db.session.add(new_order)
+        db.session.commit()
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    except exc.IntegrityError:
+        return jsonify({"message": "Invalid user ID"}), 400
+    return order_schema.jsonify(new_order), 201
+
+@app.route('/orders/<order_id>/add_product/<product_id>', methods=['PUT'])
+def add_product_to_order(order_id, product_id):
+    order = db.session.get(Order, order_id)
+    product = db.session.get(Product, product_id)
+    
+    if not order:
+        return jsonify({"message": "Invalid order id"}), 400
+    
+    if not product:
+        return jsonify({"message": "Invalid product id"}), 400
+    
+    try:
+        order.products.append(product)
+        db.session.commit()
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify({"message": f"Product {product.id} is already in the list"}), 400
+
+    return jsonify({"message": f"{product.product_name} add to the order {order.id}."}), 200
+
+@app.route('/orders/<order_id>/remove_product/<product_id>', methods=['DELETE'])
+def remove_product_from_order(order_id, product_id):
+    order = db.session.get(Order, order_id)
+    product = db.session.get(Product, product_id)
+    
+    if not order:
+        return jsonify({"message": "Invalid order id"}), 400
+    
+    if not product:
+        return jsonify({"message": "Invalid product id"}), 400
+    
+    if not product in order.products:
+        return jsonify({"message": f"Product {product.id} is not in the order {order.id}"}), 400
+
+    order.products.remove(product)
+    db.session.commit()
+    return jsonify({"message": f"{product.product_name} deleted from the order {order.id}."}), 200
+
+@app.route('/orders/user/<user_id>', methods = ['GET'])
+def get_all_orders_for_user(user_id):
+    
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    query = select(Order).filter(Order.user_id == user_id)
+    orders = db.session.execute(query).scalars().all()
+    return orders_schema.jsonify(orders), 200
+
+@app.route('/orders/<order_id>/products', methods = ['GET'])
+def get_all_products_for_order(order_id):
+    order = db.session.get(Order, order_id)
+    return jsonify({"products": products_schema.dump(order.products)}), 200
---
+++ src/api/users_api.py
@@ -0,0 +1,57 @@
+from flask import request, jsonify
+from sqlalchemy import select
+from marshmallow import ValidationError
+from config import app, db
+from models.user import User
+from schemas import user_schema, users_schema
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    query = select(User)
+    users = db.session.execute(query).scalars().all()
+    return users_schema.jsonify(users), 200
+
+@app.route('/users/<int:id>', methods = ['GET'])
+def get_user(id):
+    user = db.session.get(User, id)
+    return user_schema.jsonify(user), 200
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    new_user = User(name=user_data['name'], address=user_data['address'], email=user_data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(new_user), 201
+
+@app.route('/users/<int:id>', methods = ['PUT'])
+def update_user(id):
+    user = db.session.get(User, id)
+    
+    if not user:
+        return jsonify({"message": "Invalid user id"}), 400
+    
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    user.name = user_data['name']
+    user.address = user_data['address']
+    user.email = user_data['email']
+    db.session.commit()
+    return user_schema.jsonify(user), 200
+
+@app.route('/users/<int:id>', methods = ['DELETE'])
+def delete_user(id):
+    user = db.session.get(User, id)
+    
+    if not user:
+        return jsonify({"message": "Invalid user id"}), 400
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"Successfully deleted user {id}"}), 200
