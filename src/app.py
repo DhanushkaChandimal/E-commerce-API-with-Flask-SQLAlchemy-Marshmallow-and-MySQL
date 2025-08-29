eaf8e6ed9b979708bbe55e58d490af151796e453
Dhanushka <dhanushkamcr@gmail.com>
2025-08-29 15:00:31 -0500

    Move configurations to a config module

--- src/app.py
+++ src/app.py
@@ -1,23 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from flask import request, jsonify
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, DateTime, ForeignKey, Float, Table, Column, select, exc
 from marshmallow import ValidationError
 from datetime import datetime
 from typing import List
-
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:1234@localhost/ecommerce_api"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
-db.init_app(app)
-ma = Marshmallow(app)
+from config import Base, app, db, ma
 
 order_product = Table(
     "order_procuct",
